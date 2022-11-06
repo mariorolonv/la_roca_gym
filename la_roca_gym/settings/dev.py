c40cccc0575a8dff0d0fdcd19b011310b81ac69b
Mario Rolon <mario15rolon@gmail.com>
2022-11-06 13:56:07 -0500

    Creation of the project

---
+++ la_roca_gym/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-fe2j7vf%d9qfida(!6jby@n)h0@-)defgqlg1gc(mm#k@=2e3l'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
